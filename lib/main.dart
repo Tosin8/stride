364a41df90ad661346963a9c6b6e0d1f7c2d8085
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-02-28 18:49:21 +0100

    Create Splash Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:stride/screens/homepage.dart';
+import 'package:stride/splash.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,13 +13,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: HomeScreen()
+      home: SplashScreen()
     );
   }
 }
