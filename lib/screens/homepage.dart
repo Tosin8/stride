a1869295e394928ff6d1ecc2fca298f8437c74e0
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-02-29 14:22:36 +0100

    updating home screen with assets

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -13,14 +13,35 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        automaticallyImplyLeading: false,
         backgroundColor: Colors.transparent,
+        leading: Image.asset('assets/icons/menu_32.png'),
+        actions: [
+          // IconButton(
+          //   onPressed: (){}, 
+          //   icon: Icon(Icons.notifications))
+          Container(
+            width:25, height: 25,
+            child: Image.asset('assets/icons/bell.png')), 
+          const SizedBox(width: 10),
+        ],
       ),
       body: Padding(
         padding: const EdgeInsets.only(left: 8.0),
         child: ListView(
-          children: [
+          children:  [
            
-            Text('Collection')
+            const Text('Collection', style: TextStyle(fontSize: 25, fontWeight: FontWeight.w500),), 
+            const SizedBox(height: 3,), 
+            Container(
+              height: 250, 
+              width:MediaQuery.of(context).size.width ,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                image: const DecorationImage(image: AssetImage('assets/banner/banner_1.jpg'), 
+                fit: BoxFit.cover),
+              ),
+              )
           ],
         ),
       )
