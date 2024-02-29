48905c45b8c34a3c71be3009067cc5591fc9b313
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-02-29 13:09:48 +0100

    Update splash.dart

--- lib/splash.dart
+++ lib/splash.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class SplashScreen
  extends StatelessWidget {
@@ -28,10 +29,42 @@ class SplashScreen
               begin: Alignment.bottomCenter), 
           ),
           child: 
-          Column(
+           Column(
+            mainAxisAlignment: MainAxisAlignment.end,
             children: [
+const Text('Stride',style: TextStyle(color: Colors.white, fontSize: 30),), 
+        const SizedBox(height: 10,), 
+        const Divider(
+          height: 3, thickness: 2,indent: 100, endIndent: 100,
+          color: Colors.white,  
+        ), 
+        const SizedBox(height: 20,), 
+              const Text('Thousand of Shoe Styles\nfrom Top Brands', style: TextStyle(color: Colors.white, fontSize: 20),
+              textAlign: TextAlign.center,),
+              const SizedBox(height: 40,), 
+              GestureDetector( 
+                onTap:() {
+                  
+                },
+                child: Container(
+                  height: 50, width: 200,
+                  decoration: BoxDecoration(
+                    color: Colors.black, 
+                    borderRadius: BorderRadius.circular(10), 
+                  ),
+                child: const Align(
+                  child: Text('Start Shopping',
+                   style: TextStyle(color: Colors.white, fontWeight: FontWeight.w400,
+                   fontSize: 20),),
+                ),
+                ),
+              ), 
+              const SizedBox(height: 60,), 
 
-              Text('Thousand of Shoe Styles\nfrom Top Brands')
+
+
+
+              
             ],
           ),
         ),
