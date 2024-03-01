daa24d5869aedfbce0ac403fb84d4dffd4fde1ae
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-03-01 16:07:14 +0100

    Refactoring

--- lib/screens/home/homepage.dart
+++ lib/screens/home/homepage.dart
@@ -50,87 +50,81 @@ class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
              
               const productCarousel(), 
                
-              Container(
-                width: double.maxFinite,
-                
-              //  height: MediaQuery.of(context).size.height, 
-                child:  TabBar(
-                    tabAlignment: TabAlignment.start,
-                    isScrollable: true,
-                    controller: _tabController,
-                    labelColor: Colors.black,
-                    labelPadding: const EdgeInsets.only(left: 20, right: 20),
-                    unselectedLabelColor: Colors.grey, 
-                    indicator: CircleTabIndicator(
-                      color:Colors.black,
-                       radius: 4),
-                    tabs: const [
-                       Tab(
-                        child: Text('Popular',
-                        ),
-                      ), 
-                        Tab(
-                        child: Text('Adidas',
-                        ),
-                      ),
-                        Tab(
-                        child: Text('Jordan',
-                         ),
-                      ),
-                       Tab(
-                        child: Text('Nike',
-                        ),
-                      ),
-                       Tab(
-                        child: Text('Puma',
-                      ),
-                      ),
-                  
-                  ]),
-                ),
+              productTab(tabController: _tabController),
               
-               Container(
-                  width: double.maxFinite,
-                  height: MediaQuery.of(context).size.height,
-                  child:  TabBarView(
-                    controller: _tabController,
-                    children: [
-                     Container(
-                      width: MediaQuery.of(context).size.width, 
-                      height: MediaQuery.of(context).size.height,
-                      child: CarouselSlider(
-                        options: CarouselOptions(
-                          height: 440, 
-                        
-                          aspectRatio: 16/9, 
-                          viewportFraction: 0.70, 
-                          enlargeCenterPage: true, 
-                        ),
-                        items: List.generate(
-                          productSliders.length,
-                         (index) => productCarouselCard(
-                           productSliders: productSliders[index],
-                         ) )
-                        ),
-                     ), 
-                        const Text('Popular'),
-                      
-                       const Text('Adidas'),
-                      
-                      
-                       
-                       const Text('Nike'),
-                    
-                       const Text('Puma'),
-                      
-                    ]),
-                ),
+               productTabContents(tabController: _tabController),
               
             ],
           ),
         ),
       ); 
     
+  }
+}
+
+class productTabContents extends StatelessWidget {
+  const productTabContents({
+    super.key,
+    required TabController tabController,
+  }) : _tabController = tabController;
+
+  final TabController _tabController;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+       width: double.maxFinite,
+       height: MediaQuery.of(context).size.height,
+       child:  TabBarView(
+         controller: _tabController,
+         children: const [
+          
+             popularPage(),
+           
+          
+          
+             Text('Popular'),
+           
+             Text('Adidas'),
+           
+           
+            
+            Text('Nike'),
+         
+             Text('Puma'),
+           
+         ]),
+     );
+  }
+}
+
+
+
+class popularPage extends StatelessWidget {
+  const popularPage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width, 
+      height: MediaQuery.of(context).size.height,
+      child: CarouselSlider(
+        options: CarouselOptions(
+          height: 440, 
+        
+         // aspectRatio: 16/9, 
+          //viewportFraction: 0.70, 
+          enlargeCenterPage: true, 
+        ),
+        items: List.generate(
+          productSliders.length,
+         (index) => productCarouselCard(
+           productSliders: productSliders[index],
+         ) )
+        ),
+     );
   }
 }
 
@@ -192,16 +186,16 @@ final ProductSlider productSliders;
                     children: [
                       Container(
                         height: 40, width: 40,
-                        decoration: BoxDecoration(
+                        decoration: const BoxDecoration(
                           borderRadius: BorderRadius.only(
                             topLeft: Radius.circular(10),
                             topRight: Radius.circular(10), 
                           ),
                           color: Colors.black,
                         ),
-                        child: Icon(Icons.add, color: Colors.white,size: 30,),
+                        child: const Icon(Icons.add, color: Colors.white,size: 30,),
                       ), 
-                      SizedBox(width: 20,), 
+                      const SizedBox(width: 20,), 
                     ],
                   ) 
            ],
--- lib/screens/home/hwidget.dart
+++ lib/screens/home/hwidget.dart
@@ -1,6 +1,8 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
+import 'homepage.dart';
+
 class productCarousel extends StatelessWidget {
   const productCarousel({
     super.key,
@@ -47,5 +49,93 @@ class productCarousel extends StatelessWidget {
           ),
                  ] ),
     );
+  }
+}
+
+class productTab extends StatelessWidget {
+  const productTab({
+    super.key,
+    required TabController tabController,
+  }) : _tabController = tabController;
+
+  final TabController _tabController;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.maxFinite,
+      
+    //  height: MediaQuery.of(context).size.height, 
+      child:  TabBar(
+          tabAlignment: TabAlignment.start,
+          isScrollable: true,
+          controller: _tabController,
+          labelColor: Colors.black,
+          labelPadding: const EdgeInsets.only(left: 20, right: 20),
+          unselectedLabelColor: Colors.grey, 
+          indicator: CircleTabIndicator(
+            color:Colors.black,
+             radius: 4),
+          tabs: const [
+             Tab(
+              child: Text('Popular',
+              ),
+            ), 
+              Tab(
+              child: Text('Adidas',
+              ),
+            ),
+              Tab(
+              child: Text('Jordan',
+               ),
+            ),
+             Tab(
+              child: Text('Nike',
+              ),
+            ),
+             Tab(
+              child: Text('Puma',
+            ),
+            ),
+        
+        ]),
+      );
+  }
+}
+
+
+class productTabContents extends StatelessWidget {
+  const productTabContents({
+    super.key,
+    required TabController tabController,
+  }) : _tabController = tabController;
+
+  final TabController _tabController;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+       width: double.maxFinite,
+       height: MediaQuery.of(context).size.height,
+       child:  TabBarView(
+         controller: _tabController,
+         children: const [
+          
+             popularPage(),
+           
+          
+          
+             Text('Popular'),
+           
+             Text('Adidas'),
+           
+           
+            
+            Text('Nike'),
+         
+             Text('Puma'),
+           
+         ]),
+     );
   }
 }
