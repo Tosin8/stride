ecadcdbf2269c63dc71648bc7a0f765a22565f51
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-03-01 13:39:44 +0100

    Update productSlider.dart

--- lib/model/productSlider.dart
+++ lib/model/productSlider.dart
@@ -11,3 +11,39 @@ class ProductSlider {
     required this.price,
   });
 }
+
+
+List<ProductSlider> productSliders = [
+  ProductSlider(
+    image: "assets/products/slider/1.jpg",
+    title: "Product 1",
+    category: "Adidas",
+    price: 200,
+  ), 
+  ProductSlider(
+    image: "assets/products/slider/2.jpg",
+    title: "Product 1",
+    category: "Adidas",
+    price: 200,
+  ), ProductSlider(
+    image: "assets/products/slider/3.jpg",
+    title: "Product 1",
+    category: "Adidas",
+    price:140,
+  ), ProductSlider(
+    image: "assets/products/slider/4.jpg",
+    title: "Product 1",
+    category: "Puma",
+    price: 420,
+  ), ProductSlider(
+    image: "assets/products/slider/5.jpg",
+    title: "Product 1",
+    category: "Jordan",
+    price: 310,
+  ), ProductSlider(
+    image: "assets/products/slider/6.jpg",
+    title: "Product 1",
+    category: "Nike",
+    price: 240,
+  ), 
+];
