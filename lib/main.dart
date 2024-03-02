c2c2c6e4d4ba1b2148b29fdf4feddefef6c733ce
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-03-02 15:18:23 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,57 @@
+// import 'package:flutter/material.dart';
+
+// import 'package:stride/splash.dart';
+
+// import 'runs.dart';
+
+// void main() {
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       title: 'Stride',
+//       theme: ThemeData(
+        
+//         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+//         useMaterial3: true,
+//       ),
+//       home: const Runs(),
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
 
-import 'package:stride/splash.dart';
-
-import 'runs.dart';
-
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Stride',
-      theme: ThemeData(
-        
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const Runs(),
-    );
-  }
-}
-
+      runApp(MaterialApp(
+        home: Scaffold(
+          body: NestedScrollView(
+            headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+              return <Widget>[
+                SliverAppBar(
+                  title: const Text('NestedScrollView Example'),
+                  floating: true,
+                  pinned: true,
+                  forceElevated: innerBoxIsScrolled,
+                ),
+              ];
+            },
+            body: ListView.builder(
+              itemCount: 50,
+              itemBuilder: (BuildContext context, int index) {
+                return ListTile(
+                  title: Text('List Item $index'),
+                );
+              },
+            ),
+          ),
+        ),
+      ));
+    }
