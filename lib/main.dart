3c5eae7bd118f1bb117c87451dcd521c5b123a24
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-03-03 15:37:27 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:stride/screens/home/homepage.dart';
 
 import 'package:stride/splash.dart';
 
-import 'runs.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const HomeScreen(),
+      home: const SplashScreen(),
     );
   }
 }
