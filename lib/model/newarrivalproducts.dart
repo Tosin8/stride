eb4ef1d68ba21319bcdfcee02d47f9c124c7c457
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-03-03 17:41:14 +0100

    Creating UI for New Arrival Card

--- lib/model/newarrivalproducts.dart
+++ lib/model/newarrivalproducts.dart
@@ -3,10 +3,12 @@ class newArrivalProduct {
   String image; 
   String title;
   String category;
+  String price; 
   newArrivalProduct({
     required this.image,
     required this.title,
     required this.category,
+    required this.price, 
   });
 }
 
@@ -15,22 +17,27 @@ final List newArrivalProducts = [
     image: "assets/products/newarrival/1.jpg",
     title: "Product 1",
     category: "Adidas",
+    price: '200',
   ), newArrivalProduct(
     image: "assets/products/newarrival/2.jpg",
     title: "Product 1",
     category: "Nike",
+    price: '400',
   ), newArrivalProduct(
     image: "assets/products/newarrival/3.jpg",
     title: "Product 1",
     category: "Jordan",
+    price: '200',
   ), newArrivalProduct(
     image: "assets/products/newarrival/4.jpg",
     title: "Product 1",
     category: "Adidas",
+    price: '550',
   ),
    newArrivalProduct(
     image: "assets/products/newarrival/5.jpg",
     title: "Product 1",
     category: 'Puma'
+    ,price: '200',
    ),
 ];
