04d46be548b8cebcf3fefc5bc8d8841a845a54a2
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-03-03 16:38:20 +0100

    Update newarrivalproducts.dart

---
+++ lib/model/newarrivalproducts.dart
@@ -0,0 +1,11 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+class newArrivalProduct {
+  String image; 
+  String title;
+  String category;
+  newArrivalProduct({
+    required this.image,
+    required this.title,
+    required this.category,
+  });
+}
