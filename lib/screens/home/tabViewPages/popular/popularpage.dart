cad8b69d498329f9425eea597cfa30e43dd6afe2
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-03-08 23:56:28 +0100

    Update popularpage.dart

--- lib/screens/home/tabViewPages/popular/popularpage.dart
+++ lib/screens/home/tabViewPages/popular/popularpage.dart
@@ -108,79 +108,80 @@ class popularPage extends StatelessWidget {
 
 
 
+// ignore: camel_case_types
 class dealProductsCard extends StatelessWidget {
   const dealProductsCard({
-    Key? key, required this.DealsProducts,
+    super.key, required this.DealsProducts,
     
-  }) : super(key: key);
+  });
 
 final DealsProduct DealsProducts; 
   @override
   Widget build(BuildContext context, ) {
-    return Container(
-    height: 1500, 
-      child: ListView(
-        shrinkWrap: true,
-        children: [
-        Column( 
-        crossAxisAlignment: CrossAxisAlignment.start,
+    return  Container(
         
-          children: [
-            Container(
-              height: 150, 
-              decoration: BoxDecoration(
-              borderRadius: const BorderRadius.only(
-              topLeft: Radius.circular(10), 
-              topRight: Radius.circular(10)
-              ),
-                image: DecorationImage(
-                  image: AssetImage(
-                    DealsProducts.image
-                  ),
-                  fit: BoxFit.cover, 
-                  
+      height: 500, 
+        child: 
+          Column( 
+          crossAxisAlignment: CrossAxisAlignment.start,
+          
+            children: [
+              Container(
+                height: 150, 
+                decoration: BoxDecoration(
+                borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(10), 
+                topRight: Radius.circular(10)
                 ),
-                
-              )
-            ),
-            
-            Text(DealsProducts.title, 
-            style: const TextStyle(fontSize: 18, fontWeight: FontWeight.w700),),
-             const SizedBox(height: 5,), 
-             Text(DealsProducts.category, 
-             style: const TextStyle(color: Colors.grey),),
-             const SizedBox(height: 2,), 
-              Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-             children: [
-             Text(DealsProducts.price,
-              style: const TextStyle(
-              color: Colors.black, 
-              fontWeight: FontWeight.w700, fontSize: 18),),
-             GestureDetector(
-             onTap:() {
-               
-             },
-               child: Container(
-               height: 30, 
-               width: 30,
-               decoration: BoxDecoration(
-                 borderRadius: BorderRadius.circular(80),
-                 color: Colors.black
-               ),
-               child: const Center(
-               child: Icon(Icons.add, color: Colors.white
-               
-               
+                  image: DecorationImage(
+                    image: AssetImage(
+                      DealsProducts.image
+                    ),
+                    fit: BoxFit.cover, 
+                    
+                  ),
+                  
+                )
+              ),
+              
+              Text(DealsProducts.title, 
+              style: const TextStyle(fontSize: 18, fontWeight: FontWeight.w700),),
+               const SizedBox(height: 5,), 
+               Text(DealsProducts.category, 
+               style: const TextStyle(color: Colors.grey),),
+               const SizedBox(height: 2,), 
+                Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+               children: [
+               Text(DealsProducts.price,
+                style: const TextStyle(
+                color: Colors.black, 
+                fontWeight: FontWeight.w700, fontSize: 18),),
+               GestureDetector(
+               onTap:() {
+                 
+               },
+                 child: Container(
+                 height: 30, 
+                 width: 30,
+                 decoration: BoxDecoration(
+                   borderRadius: BorderRadius.circular(80),
+                   color: Colors.black
+                 ),
+                 child: const Center(
+                 child: Icon(Icons.add, color: Colors.white
+                 
+                 
+                 )
+                 ),
+                 ),
                )
-               ),
-               ),
-             )
-             ],)
-        
-          ]
-        ),
-    ]  )
-    );
+               ],)
+          
+            ]
+          ),
+       );
+    
+    
   }
 }
