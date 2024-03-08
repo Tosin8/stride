1be4648460fff27b4b67ac81f63856a3b0f72a0d
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-03-08 08:19:34 +0100

    Update hwidget.dart

--- lib/screens/home/hwidget.dart
+++ lib/screens/home/hwidget.dart
@@ -1,7 +1,6 @@
+
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
-
-import 'homepage.dart';
 import 'tabViewPages/popular/popularpage.dart';
 import 'tabViewPages/widgets.dart';
 
