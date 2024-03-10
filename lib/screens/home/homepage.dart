296acbee3ed1f08dcf37e0a94c76c6770b48c2b4
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-03-10 17:26:19 +0100

    Creating UI for drawer menu

--- lib/screens/home/homepage.dart
+++ lib/screens/home/homepage.dart
@@ -1,6 +1,8 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first, must_be_immutable
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:iconsax/iconsax.dart';
 
 import 'widgets/productCarousel.dart';
 import 'widgets/productTab.dart';
@@ -18,42 +20,32 @@ class HomeScreen extends StatefulWidget {
 
 final double maxSlide = 225.0; 
 class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
- late  AnimationController animationController; 
+  
 
   int _current = 0; 
   dynamic _selectdProduct = {}; 
 
-@override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    animationController = AnimationController(vsync: this,
-     duration: const Duration(milliseconds: 250), 
-    ); 
-  }
 
-  void toggle() => animationController.isDismissed ? animationController.forward() : animationController.reverse(); 
 
-  
   @override
   Widget build(BuildContext context) {
     TabController _tabController = TabController(
       length: 5, vsync: this);
-    return Stack(
+    return   Stack(
+          
+          children:[
+           
+            
+            main_body(tabController: _tabController), 
+             const navBar(), 
+            
+         ] );
+        }
       
-      children:[
-        const navBar(), 
-        
-        Transform(
-          transform: Matrix4.identity()
-          ..translate(maxSlide)
-          ..scale(0.7),
-          alignment: Alignment.centerLeft,
-          child: main_body(tabController: _tabController))
-     ] ); 
+    
     
   }
-}
+
 
 class main_body extends StatelessWidget {
   const main_body({
@@ -65,47 +57,56 @@ class main_body extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        automaticallyImplyLeading: false,
-        backgroundColor: Colors.transparent,
-        leading: Image.asset('assets/icons/menu_32.png'),
-        actions: [
-          // IconButton(
-          //   onPressed: (){}, 
-          //   icon: Icon(Icons.notifications))
-          // ignore: sized_box_for_whitespace
-          Container(
-            width:20, height: 20,
-            child: Image.asset('assets/icons/bell.png')), 
-          const SizedBox(width: 10),
-        ],
-      ),
-      body: 
-        Padding(
-          padding:  const EdgeInsets.all(8.0),
-          child: ListView( 
-            children:  [
-             
-              const Padding(
-                padding: EdgeInsets.only(left: 8.0),
-                child: Text('Collection',
-                 style: TextStyle(
-                  fontSize: 25, 
-                  fontWeight: FontWeight.w500),),
-              ), 
-             
-              const productCarousel(), 
-               
-              productTab(tabController: _tabController),
-              
-               productTabContents(tabController: _tabController),
-              
-            ],
-          ),
+
+    
+    return
+   
+      Scaffold(
+        appBar: AppBar(
+          elevation: 0,
+          automaticallyImplyLeading: false,
+          backgroundColor: Colors.transparent,
+          leading: GestureDetector(
+            child: 
+           
+            const Icon(Iconsax.menu),
+          ), 
+          actions: [
+            // IconButton(
+            //   onPressed: (){}, 
+            //   icon: Icon(Icons.notifications))
+            // ignore: sized_box_for_whitespace
+            Container(
+              width:20, height: 20,
+              child: Image.asset('assets/icons/bell.png')), 
+            const SizedBox(width: 10),
+          ],
         ),
-      );
+        body: 
+          Padding(
+            padding:  const EdgeInsets.all(8.0),
+            child: ListView( 
+              children:  [
+               
+                const Padding(
+                  padding: EdgeInsets.only(left: 8.0),
+                  child: Text('Collection',
+                   style: TextStyle(
+                    fontSize: 25, 
+                    fontWeight: FontWeight.w500),),
+                ), 
+               
+                const productCarousel(), 
+                 
+                productTab(tabController: _tabController),
+                
+                 productTabContents(tabController: _tabController),
+                
+              ],
+            ),
+          ),
+        
+    );
   }
 }
 
@@ -117,9 +118,91 @@ class navBar extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        color: Colors.blue,
+        color: Colors.black, 
+        child: Padding(
+          padding: const EdgeInsets.only(
+            
+            top: 40.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text('Stride',
+          style: TextStyle(
+            color: Colors.white, 
+            fontSize: 35, 
+            fontFamily: 'Maturascript')),  
+SizedBox(height: 20,), 
+          GestureDetector(
+            onTap: (){},
+            child: const ListTile(
+              leading: Icon(Iconsax.home, color: Colors.white), 
+              title: Text('Home', style: TextStyle(color: Colors.white),),
+            ),
+          ), 
+           GestureDetector(
+            onTap: (){},
+            child: const ListTile(
+              leading: Icon(Iconsax.shopping_cart, color: Colors.white), 
+              title: Text('Cart', style: TextStyle(color: Colors.white),),
+            ),
+          ), 
+           GestureDetector(
+            onTap: (){},
+            child: const ListTile(
+              leading: Icon(Iconsax.folder_favorite, color: Colors.white), 
+              title: Text('Favorite', style: TextStyle(color: Colors.white),),
+            ),
+          ), 
+           GestureDetector(
+            onTap: (){},
+            child: const ListTile(
+              leading: Icon(Iconsax.user, color: Colors.white), 
+              title: Text('Profile', style: TextStyle(color: Colors.white),),
+            ),
+          ), 
+            GestureDetector(
+            onTap: (){},
+            child: const ListTile(
+              leading: Icon(Iconsax.notification, color: Colors.white), 
+              title: Text('Notification', style: TextStyle(color: Colors.white),),
+            ),
+          ), 
+          SizedBox(height: 20,), 
+          Divider(
+            color: Colors.white,
+            thickness: 1,
+            indent: 0,
+            endIndent: 200,
+            ),
+             GestureDetector(
+            onTap: (){},
+            child: const ListTile(
+              leading: Icon(Iconsax.setting, color: Colors.white), 
+              title: Text('App Settings', style: TextStyle(color: Colors.white),),
+            ),
+          ), 
+           GestureDetector(
+            onTap: (){},
+            child: const ListTile(
+              leading: Icon(Iconsax.call, color: Colors.white), 
+              title: Text('Customer Care', style: TextStyle(color: Colors.white),),
+            ),
+          ), 
+          
+        
+             GestureDetector(
+            onTap: (){},
+            child: const ListTile(
+              leading: Icon(Iconsax.close_circle, color: Colors.white), 
+              title: Text('Close App', style: TextStyle(color: Colors.white),),
+            ),
+          ), 
+          ],),
+        )
+        
       ),
     );
   }
 }
+
 
