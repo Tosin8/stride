97427e361734593aea0922b4da210c6adb2a7a7f
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-10-03 16:45:11 +0100

    updating

---
+++ lib/routes/app_routes.dart
@@ -0,0 +1,13 @@
+
+
+import 'package:get/get.dart';
+import 'package:stride/screens/home/homepage.dart';
+
+import 'routes.dart';
+
+class AppRoutes {
+  static final pages = [
+    GetPage(name: BRoutes.home, page: () => const HomeScreen()), 
+      
+  ]; 
+}
