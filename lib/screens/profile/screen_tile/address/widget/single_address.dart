88e02fe98eb69cb5c9767b45a74dcbc98825f815
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-10-05 00:27:41 +0100

    up

--- lib/screens/profile/screen_tile/address/widget/single_address.dart
+++ lib/screens/profile/screen_tile/address/widget/single_address.dart
@@ -1,62 +1,62 @@
-import 'package:flutter/material.dart';
-import 'package:iconsax/iconsax.dart';
+// import 'package:flutter/material.dart';
+// import 'package:iconsax/iconsax.dart';
 
-import 'package:stride/utils/colors.dart';
-import 'package:stride/utils/constants/sizes.dart';
-import 'package:stride/utils/helpers/helpers_function.dart';
+// import 'package:stride/utils/colors.dart';
+// import 'package:stride/utils/constants/sizes.dart';
+// import 'package:stride/utils/helpers/helpers_function.dart';
 
-class SingleAddress extends StatelessWidget {
-  const SingleAddress({super.key, required this.selectedAddress});
+// class SingleAddress extends StatelessWidget {
+//   const SingleAddress({super.key, required this.selectedAddress});
 
-final bool selectedAddress;
-  @override
-  Widget build(BuildContext context) {
+// final bool selectedAddress;
+//   @override
+//   Widget build(BuildContext context) {
 
-    final dark = BHelperFunctions.isDarkMode(context);
-    return BRoundedContainer(
-      padding: const EdgeInsets.all(BSizes.md), 
-  width: double.infinity, 
-  showBorder: true, 
-  borderColor: selectedAddress ?
+//     final dark = BHelperFunctions.isDarkMode(context);
+//     return BRoundedContainer(
+//       padding: const EdgeInsets.all(BSizes.md), 
+//   width: double.infinity, 
+//   showBorder: true, 
+//   borderColor: selectedAddress ?
   
-   Colors.transparent:  dark ?   BColors.darkGrey : BColors.grey, 
-  backgroundColor: selectedAddress ? BColors.primary.withOpacity(0.5): Colors.transparent, 
-  margin: const EdgeInsets.only(bottom: BSizes.spaceBtwItems), 
+//    Colors.transparent:  dark ?   BColors.darkGrey : BColors.grey, 
+//   backgroundColor: selectedAddress ? BColors.primary.withOpacity(0.5): Colors.transparent, 
+//   margin: const EdgeInsets.only(bottom: BSizes.spaceBtwItems), 
  
-  child: Stack(
-    children: [
-      Positioned( 
-        right: 5, top: 0, 
-        child: Icon(
-        selectedAddress ? Iconsax.tick_circle5 : null, 
-        color: selectedAddress 
-        ? dark 
-        ? BColors.light : BColors.dark : null,
-        ),
-      ) , 
-       Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-           Text('John Doe', 
-          maxLines: 1, 
-          overflow: TextOverflow.ellipsis, 
-          style: Theme.of(context).textTheme.titleLarge,
-          ), 
-          const SizedBox(height: BSizes.sm /2), 
-          const Text('(+1) 123 456 7890', maxLines: 1, overflow: TextOverflow.ellipsis,), 
-          const SizedBox(height: BSizes.sm /2), 
+//   child: Stack(
+//     children: [
+//       Positioned( 
+//         right: 5, top: 0, 
+//         child: Icon(
+//         selectedAddress ? Iconsax.tick_circle5 : null, 
+//         color: selectedAddress 
+//         ? dark 
+//         ? BColors.light : BColors.dark : null,
+//         ),
+//       ) , 
+//        Column(
+//         crossAxisAlignment: CrossAxisAlignment.start,
+//         children: [
+//            Text('John Doe', 
+//           maxLines: 1, 
+//           overflow: TextOverflow.ellipsis, 
+//           style: Theme.of(context).textTheme.titleLarge,
+//           ), 
+//           const SizedBox(height: BSizes.sm /2), 
+//           const Text('(+1) 123 456 7890', maxLines: 1, overflow: TextOverflow.ellipsis,), 
+//           const SizedBox(height: BSizes.sm /2), 
 
-const Text('2312 Timmy Cavas, South Lians, Maine, 87222, USA', 
+// const Text('2312 Timmy Cavas, South Lians, Maine, 87222, USA', 
 
-softWrap: true,
-), 
-const SizedBox(height: BSizes.sm /2,), 
+// softWrap: true,
+// ), 
+// const SizedBox(height: BSizes.sm /2,), 
 
-        ],
-      )
-    ],
-  )
-);
+//         ],
+//       )
+//     ],
+//   )
+// );
 
-  }
-}
+//   }
+// }
--- lib/screens/profile/screen_tile/orders/orders.dart
+++ lib/screens/profile/screen_tile/orders/orders.dart
@@ -1,25 +1,25 @@
-import 'package:flutter/material.dart';
-import 'package:qwibix/common/widgets/appbar/appbar.dart';
-import 'package:qwibix/features/shop/screens/profile/screen_tile/orders/widget/order_list.dart';
-import 'package:qwibix/utils/constants/sizes.dart';
+// import 'package:flutter/material.dart';
 
-class OrderScreen extends StatelessWidget {
-  const OrderScreen({super.key});
+// import 'package:stride/screens/profile/screen_tile/orders/widget/order_list.dart';
+// import 'package:stride/utils/constants/sizes.dart';
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold( 
-      appBar: BAppBar(
-        showBackArrow: true,
-        title: Text('My Orders',
-        style: Theme.of(context).textTheme.headlineSmall,),), 
+// class OrderScreen extends StatelessWidget {
+//   const OrderScreen({super.key});
 
-        body: const Padding(padding: EdgeInsets.all(BSizes.defaultSpace), 
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold( 
+//       appBar: AppBar(
+//         //showBackArrow: true,
+//         title: Text('My Orders',
+//         style: Theme.of(context).textTheme.headlineSmall,),), 
+
+//         body: const Padding(padding: EdgeInsets.all(BSizes.defaultSpace), 
         
-        // Orders
-        child: OrderListItems(), 
+//         // Orders
+//         child: OrderListItems(), 
         
-        ),
-    );
-  }
-}
+//         ),
+//     );
+//   }
+// }
