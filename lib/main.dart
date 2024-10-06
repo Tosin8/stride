f8c6df2deef4289864b07508aae75396290e6e45
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-10-06 19:09:29 +0100

    connecting to firebase.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:stride/firebase_options.dart';
 
 import 'package:stride/splash.dart';
 
 
 
-void main() {
+void main() async { 
+
+  WidgetsFlutterBinding.ensureInitialized(); 
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform); 
   runApp(const MyApp());
 }
 
