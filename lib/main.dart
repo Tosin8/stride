b62c366256412aeb0ca0b3337149678d7bb7a0b2
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-10-06 23:01:50 +0100

    io

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,36 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 import 'package:stride/firebase_options.dart';
 
 import 'package:stride/screens/splash.dart';
 
+import 'auth/repository/auth_repo.dart';
+import 'bindings/general_binding.dart';
+import 'routes/app_routes.dart';
+
 
 
 void main() async { 
-
+/// widgets binding 
+  
+  final WidgetsBinding widgetsBinding = 
   WidgetsFlutterBinding.ensureInitialized(); 
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform); 
+  /// getx local storage 
+  await GetStorage.init();
+
+  /// await splash until other items load
+
+FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+
+/// iniitalize firebase and auth. repository
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform).then(
+  (FirebaseApp value) => Get.put(AuthenticationRepository()), 
+  ); 
+
+  // load all the material design; 
   runApp(const MyApp());
 }
 
@@ -28,7 +48,15 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const SplashScreen(),
+      initialBinding: GeneralBindings(),
+      getPages: AppRoutes.pages,
+      home:
+      const Scaffold(
+        backgroundColor: Colors.white, 
+        body: Center(
+          child: CircularProgressIndicator(
+            color: Colors.white),))
+      // const SplashScreen(),
     );
   }
 }
