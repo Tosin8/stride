864901f652d7a1c98b19b4b82145dca832b3f907
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-10-13 05:18:14 +0100

    creating home ui

--- lib/screens/shop/home/home.dart
+++ lib/screens/shop/home/home.dart
@@ -12,8 +12,12 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+
+      // AppBar. 
+      backgroundColor: Colors.grey.shade100,
       appBar: AppBar(
-      leading: const Icon(Iconsax.menu),  
+           backgroundColor: Colors.grey.shade100,
+        leading: const Icon(Iconsax.menu),
         flexibleSpace: const Padding(
           padding: EdgeInsets.only(top: 62),
           child: Row(
@@ -21,39 +25,129 @@ class _HomeScreenState extends State<HomeScreen> {
             children: [
               Icon(Iconsax.location),
               SizedBox(width: 5),
-              Text('Lagos, Nigeria', style: TextStyle(color: Colors.black),),
+              Text(
+                'Lagos, Nigeria',
+                style: TextStyle(color: Colors.black),
+              ),
             ],
           ),
         ),
-      actions: [
-        GestureDetector( 
-          onTap: () {},
-          child: Stack(
-            fit: StackFit.passthrough,
-            clipBehavior: Clip.none,
-            children:[
-             const Icon(Iconsax.notification), 
-             Positioned(
-               top: -8, right: -3,
-               child: Container(
-                width: 18, height: 18,
-                decoration: const BoxDecoration(
-                  
-                  shape: BoxShape.circle, 
-                  color: Colors.black
+        actions: [
+          GestureDetector(
+            onTap: () {
+              print('notification tapped');
+            },
+            child: Stack(
+              fit: StackFit.passthrough,
+              clipBehavior: Clip.none,
+              children: [
+                const Icon(Iconsax.notification),
+                Positioned(
+                  top: -8,
+                  right: -3,
+                  child: Container(
+                    width: 18,
+                    height: 18,
+                    decoration: const BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.black,
+                    ),
+                    child: const Center(
+                        child: Text(
+                      '2',
+                      style: TextStyle(color: Colors.white),
+                    )),
+                  ),
+                )
+              ],
+            ),
+          ),
+          const SizedBox(width: 7),
+          Stack(
+              fit: StackFit.passthrough,
+              clipBehavior: Clip.none,
+              children: [
+                const Icon(Iconsax.shopping_cart),
+                Positioned(
+                  top: -8,
+                  right: -3,
+                  child: Container(
+                    width: 18,
+                    height: 18,
+                    decoration: const BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.black,
+                    ),
+                    child: const Align(
+                        child: Text(
+                      '2',
+                      style: TextStyle(color: Colors.white),
+                    )),
+                  ),
+                )
+              ]),
+          const SizedBox(width: 15),
+        ],
+        bottom: PreferredSize(
+          preferredSize: const Size.fromHeight(65),
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 16),
+                    height: 50,
+                    decoration: BoxDecoration(
+                      color: Colors.grey[200],
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    child: Row(
+                      children: [
+                        const Icon(Iconsax.search_normal),
+                        const SizedBox(width: 10),
+                        Expanded(
+                          child: TextFormField(
+                            keyboardType: TextInputType.text,
+                          textInputAction: TextInputAction.search,
+                            onFieldSubmitted: (value) {
+                              
+                            },
+                            decoration: const InputDecoration(
+                              border: InputBorder.none,
+                              hintText: "Search your sneakers",
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
                 ),
-                child: const Center(child: Text('2', style: TextStyle(color: Colors.white),)),),
-             )
-          ], ),
+                const SizedBox(width: 10),
+                Container(
+                  height: 45,
+                  width: 45,
+                  decoration: const BoxDecoration(
+                    color: Colors.black,
+                    shape: BoxShape.circle,
+                  ),
+                  child: const Icon(
+                    Iconsax.sort,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
-        const SizedBox(width: 7,),
-        const Stack(children:[
-          Icon(Iconsax.shopping_cart)
-        ] ), 
+      ),
 
-        const SizedBox(width: 15,), 
-      ],
-        ),
+      // Body.
+      body: const SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(15.0),
+        )
+      ),
     );
   }
 }
