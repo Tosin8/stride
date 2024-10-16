ffea35592124c700c45cdd41d9a112be4ab66cba
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-10-16 17:48:17 +0100

    updating profile

--- lib/screens/shop/home/profile/profile.dart
+++ lib/screens/shop/home/profile/profile.dart
@@ -1,7 +1,9 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:get/get_connect/http/src/utils/utils.dart';
 import 'package:iconsax/iconsax.dart';
+
 import 'package:stride/screens/shop/home/profile/usertile/personal_data.dart';
 
 class UserProfile extends StatelessWidget {
@@ -25,12 +27,13 @@ class UserProfile extends StatelessWidget {
                 subtitle: Text('johndoe@gmail.com'),
               ), 
           
-              const SizedBox(height: 12,) , 
+              const SizedBox(height: 4,) , 
               const Divider(), 
-              const SizedBox(height: 10,), 
+              const SizedBox(height: 4,), 
           
               // personal data
               Usertile(
+                subtitle: 'Manage your account details here',
                 onTap: (){
                   Get.to(() => const PersonalData()); 
                 }, 
@@ -39,50 +42,76 @@ class UserProfile extends StatelessWidget {
           // payment card
            Usertile(
                 onTap: (){}, 
+                subtitle: 'Securely store and manage your payment methods.',
                 text: 'Payment Cards', leading: Iconsax.card, ), 
                 // settings 
                 Usertile(
+                  subtitle: 'Customize your app preferences, privacy and options.',
                 onTap: (){}, 
                 text: 'Settings', leading: Iconsax.setting, ), 
                 
                 // cart
                 Usertile(
+                  subtitle: 'View your purchase history and track orders',
                 onTap: (){}, 
                 text: 'Orders ', leading: Iconsax.shopping_bag, ),
                 
                 // notification
                  Usertile(
+                  subtitle: 'Stay updated with important alerts and messages.',
                 onTap: (){}, 
                 text: 'Notifications', leading: Iconsax.message, ), 
 
                 // pending review
                  Usertile(
+                  subtitle: 'Review and rate your recent purchases.',
                 onTap: (){}, 
                 text: 'Pending Reviews', leading: Iconsax.activity, ), 
                 
 
-                const SizedBox(height: 12,) , 
+                const SizedBox(height: 4,) , 
               const Divider(), 
-              const SizedBox(height: 10,), 
+              const SizedBox(height: 4,), 
           
               // personal data
               Usertile(
+                subtitle: 'Find answers to common questions and issues',
                 onTap: (){}, 
               text: 'FAQs', leading: Icons.question_answer, ), 
           
                 // settings 
                 Usertile(
+                  subtitle: 'Get help and support for any inquiries.',
                 onTap: (){}, 
                 text: 'Customer Care', leading: Icons.phone, ),
 
-                const SizedBox(height: 12,) , 
+                const SizedBox(height: 4,) , 
               const Divider(), 
               const SizedBox(height: 10,), 
           
               // personal data
               Usertile(
+                subtitle: 'Log out of your account.',
                 onTap: (){}, 
               text: 'Log Out', leading: Icons.exit_to_app, ),
+              const SizedBox(height: 10,), 
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: GestureDetector(
+                  onTap: (){},
+                  child: Container(
+                    height: 50,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8),
+                      border: Border.all(
+                        color: Colors.red, 
+                      )
+                    ),
+                    child: const Align(child: Text('Delete Account', style: TextStyle(color: Colors.red, fontSize: 16, fontWeight: FontWeight.bold),)),
+                  ),
+                ),
+              ), 
             ],
           ),
         ),
@@ -95,12 +124,13 @@ class Usertile extends StatelessWidget {
   const Usertile({
     super.key,
     required this.text,
+    required this.subtitle,
     this.onTap,
     required this.leading,
-  
   });
 
 final String text; 
+final String subtitle; 
 final void Function()? onTap; 
 final IconData leading; 
 
@@ -121,6 +151,7 @@ final IconData leading;
           ),
           child: Icon(leading, color: Colors.black,)), 
         title: Text(text, style: const TextStyle(fontWeight: FontWeight.w500),),
+        subtitle: Text(subtitle, style: const TextStyle(fontSize: 13),),
         
         trailing: const Icon(Iconsax.arrow_right),
       ),
--- lib/screens/shop/home/profile/usertile/personal_data.dart
+++ lib/screens/shop/home/profile/usertile/personal_data.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:stride/utils/validators/validations.dart';
 
 class PersonalData extends StatefulWidget {
   const PersonalData({super.key});
@@ -17,8 +20,150 @@ class _PersonalDataState extends State<PersonalData> {
           centerTitle: true,
         ),
 
-        body: const Column(
-          children: [],
+        body:  ListView(
+          scrollDirection: Axis.vertical,
+          // mainAxisAlignment: MainAxisAlignment.start, crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const SizedBox(height: 6,), 
+            Center(
+              child: Stack(
+                clipBehavior: Clip.none,
+                children:[ const CircleAvatar(
+                  radius: 40,
+                  child: Icon(Iconsax.user),
+                ),
+                Positioned(
+                  bottom: 0,
+                  right: -18,
+                  child: IconButton(onPressed: (){}, icon: const Icon(Iconsax.edit, size: 20,)))
+                        ]),
+            ), 
+
+          // Form. 
+          const SizedBox(height: 10),
+                    
+                      Padding(
+                        padding: const EdgeInsets.all(20.0),
+                        child: Form(
+                          child: Column(
+                            children: [
+
+                              // the name of the user will appear as hint text  
+                              TextFormField(
+                                                   //   controller: controller.email,
+                               // validator: (value) => BValidator.validateEmail(value),
+                                textInputAction: TextInputAction.next,
+                                keyboardType: TextInputType.emailAddress,
+                                decoration: const InputDecoration(
+                                  labelText: 'First Name',
+                                  prefixIcon: Icon(Iconsax.user),
+                                  border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                                  ),
+                                ),
+                              ),
+                              const SizedBox(height: 20,), 
+                              TextFormField(
+                                                   //   controller: controller.email,
+                               // validator: (value) => BValidator.validateEmail(value),
+                                textInputAction: TextInputAction.next,
+                                keyboardType: TextInputType.emailAddress,
+                                decoration: const InputDecoration(
+                                  labelText: 'Last Name',
+                                  prefixIcon: Icon(Iconsax.user),
+                                  border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                                  ),
+                                ),
+                              ),
+                              const SizedBox(height: 20,), 
+                              TextFormField(
+                                                   //   controller: controller.email,
+                               validator: (value) => BValidator.validatePhoneNumber(value),
+                                textInputAction: TextInputAction.next,
+                                keyboardType: TextInputType.number,
+                                decoration: const InputDecoration(
+                                  labelText: 'Phone Number',
+                                  prefixIcon: Icon(Icons.phone),
+                                  border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                                  ),
+                                ),
+                              ),
+                              const SizedBox(height: 20,), 
+                                 TextFormField(
+                                                   //   controller: controller.email,
+                               //validator: (value) => BValidator.validatePhoneNumb(value),
+                                textInputAction: TextInputAction.next,
+                                keyboardType: TextInputType.number,
+                                decoration: const InputDecoration(
+                                  labelText: 'Home Address',
+                                  prefixIcon: Icon(Icons.location_on),
+                                  border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                                  ),
+                                ),
+                              ),
+                               const SizedBox(height: 20,), 
+                                 TextFormField(
+                                                   //   controller: controller.email,
+                               //validator: (value) => BValidator.validatePhoneNumb(value),
+                                textInputAction: TextInputAction.next,
+                                keyboardType: TextInputType.name,
+                                decoration: const InputDecoration(
+                                  labelText: 'Office Address',
+                                  prefixIcon: Icon(Icons.location_on),
+                                  border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                                  ),
+                                ),
+                              ),
+                              const SizedBox(height: 20,), 
+                              const Divider(), 
+                               const SizedBox(height: 20,), 
+                                 TextFormField(
+                                                   //   controller: controller.email,
+                               //validator: (value) => BValidator.validatePhoneNumb(value),
+                                textInputAction: TextInputAction.next,
+                                keyboardType: TextInputType.number,
+                                decoration: const InputDecoration(
+                                  labelText: 'Change Password',
+                                  prefixIcon: Icon(Icons.location_on),
+                                  border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                                  ),
+                                ),
+                              ),
+                               const SizedBox(height: 20,), 
+                                 TextFormField(
+                                                   //   controller: controller.email,
+                               //validator: (value) => BValidator.validatePhoneNumb(value),
+                                textInputAction: TextInputAction.next,
+                                keyboardType: TextInputType.number,
+                                decoration: const InputDecoration(
+                                  labelText: 'Confirm Password',
+                                  prefixIcon: Icon(Icons.location_on),
+                                  border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                                  ),
+                                ),
+                              ),
+                              const SizedBox(height: 20,), 
+                              Container(
+                                height: 55,
+                                decoration: const BoxDecoration(
+                                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                                  color: Colors.black, 
+                                ),
+                                child: const Align(child: Text('Update Info', style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 18),),),
+                              )
+                            ],
+                          ),
+                          
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+          ],
         )
       ),
     );
